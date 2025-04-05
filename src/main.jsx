3701d0034106b863d08cb7b6d35e5bfbd06aea25
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2025-04-06 01:40:08 +0300

    base

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "modern-normalize";
+import App from "./components/App.jsx";
+import { Toaster } from "react-hot-toast";
+
+import { Provider } from "react-redux";
+//import { persistor, store } from "./redux/store"; // 2. Імпортуємо створений стор; persistor
+//import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./redux/store.js";
+
+createRoot(document.getElementById("root")).render(
+  // <StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <App />
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 2000,
+        }}
+      />
+      {/* </PersistGate> */}
+    </BrowserRouter>
+  </Provider>
+  // </StrictMode>
+);
