8a9c6d518f9225ca5e35d488b83c4bfb1e99b021
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2025-04-07 10:20:20 +0300

    sprite.svg

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,13 @@
 import { NavLink } from "react-router-dom";
+import sprite from "../../assets/sprite.svg";
+import s from "./Logo.module.css";
 
 const Logo = () => {
   return (
     <NavLink to="/">
-      Rental<span>Car</span>
+      <svg className={s.svg}>
+        <use href={`${sprite}#icon-Logo`} />
+      </svg>
     </NavLink>
   );
 };
