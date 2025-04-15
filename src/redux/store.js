e087d2b8eecac7c6d3dd8b5eada77df6953f5418
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2025-04-15 09:41:18 +0300

    persist favorite cars

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,42 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { carReducer } from "./cars/slice";
+import { carsReducer } from "./cars/slice";
 import { filterReducer } from "./filters/slice";
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; //persist
+
+//persist
+const persistConfig = {
+  key: "favoritePersistor", // як буде записано в LocalStorage
+  version: 1,
+  storage,
+  whitelist: ["favoriteCars"], // властивість state.car.favoriteCars
+};
+
+//обгортка persist
+const persistedCarsReducer = persistReducer(persistConfig, carsReducer);
 
 export const store = configureStore({
   reducer: {
-    car: carReducer,
+    car: persistedCarsReducer,
     filter: filterReducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export let persistor = persistStore(store);
