eb02ea21c2caacc792a6b07c94c9cf6a3f0dd81e
NataliaStupka <84662491+NataliaStupka@users.noreply.github.com>
2025-04-19 00:20:33 +0300

    favorite cars page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,13 @@ import HomePage from "../pages/HomePage/HomePage";
 import CatalogPage from "../pages/CatalogPage/CatalogPage";
 import CarDetailsPage from "../pages/CarDetailsPage/CarDetailsPage";
 import FavoritePage from "../pages/FavoritePage/FavoritePage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
       <Header />
+      <Toaster position="top-right" />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/catalog" element={<CatalogPage />} />
--- src/pages/FavoritePage/FavoritePage.jsx
+++ src/pages/FavoritePage/FavoritePage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { selectFavoriteCars } from "../../redux/cars/selectors";
 import CarCatalog from "../../components/CarCatalog/CarCatalog";
+import s from "./FavoritePage.module.css";
 
 const FavoritePage = () => {
   const favoriteCars = useSelector(selectFavoriteCars);
@@ -13,7 +14,11 @@ const FavoritePage = () => {
   return (
     <>
       <main style={{ paddingTop: "60px" }}>
-        <CarCatalog cars={favoriteCars} />
+        {favoriteCars.length === 0 ? (
+          <p className={s.noFavorites}>You don't have any favorite cars yet.</p>
+        ) : (
+          <CarCatalog cars={favoriteCars} />
+        )}
       </main>
     </>
   );
